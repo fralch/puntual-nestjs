070e42cc8195e06e7feb88bf527e46efc35cac41
FrankCairampoma <ingfralch@gmail.com>
2024-04-03 17:43:32 -0500

    Agregada funcionalidad para guardar la foto de asistencia en el sistema

--- src/asistencias/asistencias.service.ts
+++ src/asistencias/asistencias.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import fs from 'fs';
 import {Registro_asistencias} from '@prisma/client'
 
 @Injectable()
@@ -43,14 +44,40 @@ export class AsistenciasService {
         });
     }
 
-    async asistencias_create(id: number, hora: string, turno: string, foto: string) {
-         const fecha = new Date();
+    async asistencias_create(id: number, horaString: string, turno: string, foto: string) {
+        let fechaActual = new Date();
+        fechaActual.setTime(fechaActual.getTime() - (5 * 60 * 60 * 1000));
+        
+        // Formatear la fecha y hora actual
+        let fechaActualFormateada = fechaActual.toISOString().slice(0, 19).replace('T', ' ');
+        
+        // Formatear la fecha corta actual
+        let fechaCortaActual = fechaActual.toISOString().slice(0, 10);
+        
+        // Obtener la hora actual
+        let hora = fechaActualFormateada.slice(11, 13);
+        
+        // Obtener los minutos actuales
+        let min = fechaActualFormateada.slice(14, 16);
+        
+        // Obtener el día de la semana
+        let dias = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        let fecha_dia = dias[fechaActual.getDay()];
          
+        // console.log(fechaActual);
+        // console.log(fechaActualFormateada);
+        // console.log(fechaCortaActual);
+        // console.log(hora);
+        // console.log(min);
+        // console.log(fecha_dia);
+
+        const fotosave= foto.replace(/^data:image\/png;base64,/, "");
+        fs.writeFileSync(`./fotos/${id}-${fechaCortaActual}-${hora}-${min}.png`, fotosave, 'base64');
 
         return this.prisma.registro_asistencias.create({
         data: {
-            fecha: fecha,
-            hora_entrada: hora,
+            fecha: fechaActual,
+            hora_entrada: horaString,
             usuario_id: id,
             turno: turno,
             foto: foto,
